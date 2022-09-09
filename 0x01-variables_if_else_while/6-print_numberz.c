66de1a4c115841d439088f88afca6ee5e0e802b4
Timothy Ademola <timife007@gmail.com>
2022-09-09 06:48:12 -0700

    numberz

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -2,7 +2,7 @@
 #include <stdio.h>
 /**
  * main - entry point
- * 
+ *
  * Return: alway returns 0
  *
  */
@@ -10,9 +10,9 @@ int main(void)
 {
 	int a = 0;
 
-	for(;a < 10; a++)
+	for (; a < 10; a++)
 	{
-		putchar('0'+ a);
+		putchar('0' + a);
 	}
 	putchar('\n');
 	return (0);
