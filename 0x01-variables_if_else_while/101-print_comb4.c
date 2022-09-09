aed26e49bf0a3e447e9c06f06212b2d357e93f96
Timothy Ademola <timife007@gmail.com>
2022-09-09 07:17:09 -0700

    comb4

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -11,18 +11,18 @@ int main(void)
 	int j;
 	int k;
 
-	for(i = 0; i <= 9; i++)
+	for (i = 0; i <= 9; i++)
 	{
-		for(j = 0; j <= 9; j++)
+		for (j = 0; j <= 9; j++)
 		{
-			for(k = 0; k <= 9; k++)
+			for (k = 0; k <= 9; k++)
 			{
-				if(k > j && j > i)
+				if (k > j && j > i)
 				{
 					putchar(i + '0');
 					putchar(j + '0');
 					putchar(k + '0');
-					if(i != 7 || j != 8 || k != 9)
+					if (i != 7 || j != 8 || k != 9)
 					{
 						putchar(',');
 						putchar(' ');
