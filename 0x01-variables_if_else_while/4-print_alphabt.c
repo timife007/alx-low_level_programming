bddfe76ec957fa4b2cb15fa8d5b23455e578d699
Timothy Ademola <timife007@gmail.com>
2022-09-09 02:30:25 -0700

    alphabt

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -9,8 +9,15 @@ int main(void)
 {
 	char a;
 
-	for (a = 'a'; a <= 'z' ; a++)
+	for(a = 'a'; a <= 'z'; a++)
+	{
+		if(a == 'q' || a == 'e')
+		{
+			continue;
+		}
 		putchar(a);
+	}
 	putchar('\n');
 	return (0);
+
 }
